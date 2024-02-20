0b1e46be4011c85410ad2789660c5f71f06373f7
Ritterrh <110771711+Ritterrh@users.noreply.github.com>
2024-02-20 11:25:12 +0100

    init

---
+++ js/client.js
@@ -0,0 +1,116 @@
+﻿//GLOBAL VARIABLES on scene.js
+/*
+var data;
+var player1;
+var player2;
+var canvas;
+var engine;
+var scene;
+var spheres = [];
+var toDelete = [];
+var enemyScore = 0;
+var playerScore = 0;
+*/
+//Auch global
+var socket;
+var playerColor;
+const delay = ms => new Promise(res => setTimeout(res, ms));
+/* 
+ Spiel wird initialisiert auf der Client seite 
+*/
+function initSocket() {
+    document.getElementById("play").style.visibility = "hidden";
+    socket = null;
+    //Ist verbunden 
+    socket = io().connect();
+    document.getElementById("status").textContent = "Verbindung zum Spiel wird hergestellt";
+    socket.on('connect', function () {
+        document.getElementById("status").textContent = "Warten auf anderen Spieler";
+        playerColor = new BABYLON.Color3(Math.random(), Math.random(), Math.random());
+        startBabylonEngine(playerColor); //on baylonMultiplayer.js
+        var playerData = {
+            x: player1.position.x,
+            y: player1.position.y,
+            z: player1.position.z,
+            color: playerColor
+        }
+        socket.emit('start', playerData);
+    });
+
+    socket.on('handshake1', function (data) {
+        if (player2) {
+            player2.dispose();
+        }
+        player2 = createPlayer(scene, data.color, data);
+
+        var playerData = {
+            x: player1.position.x,
+            y: player1.position.y,
+            z: player1.position.z,
+            color: playerColor
+        }
+        socket.emit('handshake2', playerData);
+    });
+
+    socket.on('handshake3', function (data) {
+        player2 = createPlayer(scene, data.color, data);
+    });
+
+    socket.on('setSpheres', function (coord) {
+        document.getElementById("status").textContent = "Spiel läuft";
+        setSpheres(coord);
+    });
+
+    socket.on('receiveUpdate', function (data) {
+
+        player2.position.x = data.x;
+        player2.position.y = data.y;
+        player2.position.z = data.z;
+
+        if (data.left) {
+            player2.rotation.y -= 0.01;
+        } else if (data.right) {
+            player2.rotation.y += 0.01;
+        }
+
+        player2.position.x -= Math.cos(player2.rotation.y) * data.speed;
+        player2.position.z += Math.sin(player2.rotation.y) * data.speed;
+
+    });
+
+    socket.on('spherePicked', function (sphereID) {
+        var spherePicked = scene.getMeshByID(sphereID);
+        if (spherePicked) {
+            spherePicked.dispose();
+        }
+        enemyScore += 1;
+        document.getElementById("enemyScore").textContent = "Enemy : " + enemyScore + "/25";
+        if (enemyScore > 24) {
+            gameOver("LOOSE");
+        }
+    });
+
+    socket.on('bye', function (data) {
+        if (player2) {
+            player2.dispose()
+            document.getElementById("status").textContent = "Warten auf anderen Spieler. ";
+            delay(1000)
+            document.getElementById("status").textContent = "Du wirst in 5 sec neu in die warte schlange eingereit";
+            delay(100)
+            window.location.reload
+        }
+        for (i = 0; i < spheres.length; i++) {
+            spheres[i].dispose();
+        }
+
+        playerScore = 0;
+        enemyScore = 0;
+        document.getElementById("playerScore").textContent = "You : " + playerScore + "/25";
+        document.getElementById("enemyScore").textContent = "Enemy : " + enemyScore + "/25";
+    });
+
+    socket.on('machtmaking', function (data) {
+
+    })
+
+}
---
+++ server.js
@@ -0,0 +1,113 @@
+﻿var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+
+app.use(express.static(__dirname + '/'));
+
+var roomQueue = [];
+var roomQueueFull = [];
+var counterRooms = 0;
+//key:value  --> key:client , value:room's client
+var usersConnected = {};
+
+//Socket = Server ?
+
+//Socket = Server !
+io.on('connection', function (socket) {
+
+    //Sinvoller Console Log
+    console.log("Client " + socket.id + " connected");
+
+    //startet den server
+    socket.on('start', function (playerData) {
+
+        //Erstellt Räume
+        counterRooms += 1;
+        roomName = "room" + counterRooms;
+        usersConnected[socket.id] = roomName;
+
+        //Wenn kein Räume da sind 
+        if (roomQueue[0]) {
+
+            //Erstellt ein Räume
+            var room = roomQueue.shift();
+            var clientsInRoom = io.sockets.adapter.rooms[room];
+
+            //Wenn Raum ist Voll 0 und 1 also wenn zwei spieler da 
+            if (clientsInRoom.length == 1) {
+
+                //Raum wird auf die Raum Voll liste gepusht
+                roomQueueFull.push(room);
+            }
+
+            //Verbindet Client mit Raum
+            usersConnected[socket.id] = room;
+            socket.join(room);
+
+            //Sendet dem Info das er in einem Raum ist
+            socket.broadcast.to(room).emit('handshake1', playerData);
+        } else {
+
+            //Wenn Räum nicht voll sind wird der nächste Client mit einem nicht vollen raum verbunden
+            roomQueue.push(usersConnected[socket.id]);
+            socket.join(roomName);
+        }
+    });
+
+    socket.on('handshake2', function (playerData) {
+        socket.broadcast.to(usersConnected[socket.id]).emit('handshake3', playerData);
+        var coords = [];
+        var i;
+        for (i = 0; i < 50; i++) {
+            var c = {
+                x: Math.floor(Math.random() * 1001) - 500,
+                y: 2.5,
+                z: Math.floor(Math.random() * 1001) - 500
+            }
+            coords.push(c);
+        }
+        socket.broadcast.to(usersConnected[socket.id]).emit('setSpheres', coords);
+        coords.forEach(function (coord) {
+            coord.x = -coord.x;
+            coord.z = -coord.z;
+        });
+        socket.emit('setSpheres', coords);
+    });
+
+    socket.on('sendUpdate', function (data) {
+        socket.broadcast.to(usersConnected[socket.id]).emit('receiveUpdate', data);
+    });
+
+    socket.on('hit', function (idSphere) {
+        socket.broadcast.to(usersConnected[socket.id]).emit('spherePicked', idSphere);
+    });
+
+    socket.on('gameOver', function () {
+        socket.disconnect();
+    });
+
+    socket.on('disconnect', function () {
+        console.log("Client " + socket.id + " disconnected");
+        var roomClientRemoved = usersConnected[socket.id];
+        delete usersConnected[socket.id];
+
+        var index = roomQueueFull.indexOf(roomClientRemoved);
+        if (index > -1) {
+            roomQueueFull.splice(index, 1);
+            roomQueue.push(roomClientRemoved);
+        } else {
+            index = roomQueue.indexOf(roomClientRemoved)
+            if (index > -1) {
+                roomQueue.splice(index, 1)
+            }
+        }
+        socket.broadcast.to(roomClientRemoved).emit('connect', "");
+    });
+});
+
+server.listen(8001, 'localhost', function () {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("\nServer running http://%s:%s", host, port)
+});
